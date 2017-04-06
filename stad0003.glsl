ed670e7b91820b27f19d3b0bbf4c1136fa74b5b7
Tomasz Dąbrowski <dabroz@gmail.com>
2017-04-06 20:22:52 +0200

    Update stad0003.glsl

--- stad0003.glsl
+++ stad0003.glsl
@@ -6,7 +6,11 @@ const float WIDTH = 0.03;
 const float PI = 3.14159265359;
 const float WIGGLE_REPEAT = 8.0;
 const float WIGGLE_STRENGTH = 0.12;
-const float WIGGLE_OFFSET = 1.0;
+const float WIGGLE_OFFSET = PI*0.5;
+
+const vec3 COLOR_R = vec3(191.0,49.0,107.0)/255.0;
+const vec3 COLOR_G = vec3(67.0,177.0,50.0)/255.0;
+const vec3 COLOR_B = vec3(81.0,99.0,145.0)/255.0;
 
 vec3 shape(float d, vec3 radii)
 {
@@ -23,7 +27,7 @@ vec3 pattern(vec2 p)
     
     vec3 offsets = vec3(0.0, 1.0, 2.0) * WIGGLE_OFFSET;
     vec3 angles = offsets + vec3(a * WIGGLE_REPEAT);
-    float factor = sin(a + iGlobalTime * 2.0) * 0.5 + 0.5;
+    float factor = sin(a + iGlobalTime *1.0* 2.0) * 0.5 + 0.5;
     vec3 mods = sin(angles) * factor * WIGGLE_STRENGTH + vec3(1.0);
     vec3 radii = mods * vec3(RADIUS);
     
@@ -37,5 +41,13 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     uv -= vec2(1.0);
     uv.x *= iResolution.x / iResolution.y;
     
-    fragColor = vec4(pattern(uv), 1.0);
+    //uv*=0.5;
+    //uv.y+=0.5;
+    
+    vec3 c = pattern(uv);
+    vec3 ret = 
+        COLOR_R * c.x + 
+        COLOR_G * c.y +
+        COLOR_B * c.z;
+    fragColor = vec4(ret, 1.0);
 }
