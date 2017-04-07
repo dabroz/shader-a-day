97d607fc0009f7712430564652a43a5a7dab499c
Tomasz Dąbrowski <dabroz@gmail.com>
2017-04-07 22:39:43 +0200

    Update stad0004.glsl

--- stad0004.glsl
+++ stad0004.glsl
@@ -4,7 +4,7 @@
 const float NUMBER_OF_CIRCLES = 12.0;
 const float PI = 3.14159265359;
 const float MOTION_BLUR = 5.0;
-const float MAX_T = 2.0 * PI;
+const float MAX_T = 4.0;
 
 float circle(vec2 p, vec2 center, float exRadius, float inRadius)
 {
@@ -15,8 +15,10 @@ float circle(vec2 p, vec2 center, float exRadius, float inRadius)
         -smoothstep(exRadius - aaf, exRadius, d);
 }
 
-float circleEx(vec2 p, vec2 center, float exRadius, float inRadius, float t, float minBorder)
+float circleEx(vec2 p, vec2 center, float exRadius, float inRadius, float t, float speed, float minBorder)
 {
+    float regCircle = circle(p, center, exRadius, inRadius);
+    
     vec2 diff = p - center;
     float angle = atan(diff.y, diff.x);
     
@@ -28,23 +30,24 @@ float circleEx(vec2 p, vec2 center, float exRadius, float inRadius, float t, flo
     
     for (float i = 0.0; i < NUMBER_OF_CIRCLES; i++)
     {
-        float a = -t + i / NUMBER_OF_CIRCLES * PI * 2.0;
+        float a = -t*speed + i / NUMBER_OF_CIRCLES * PI * 2.0;
        
         vec2 cpos = center + vec2(cos(a), sin(a)) * xradius;
         
         float c = circle(p, cpos, recExRadius, recInRadius);
         ret = max(ret, c);
     }
-    return ret;
+    
+    return mix(ret,regCircle,smoothstep(3.0,4.0,speed));
 }
 
-float circleExMB(vec2 p, vec2 center, float exRadius, float inRadius, float t, float minBorder)
+float circleExMB(vec2 p, vec2 center, float exRadius, float inRadius, float t, float speed, float minBorder)
 {
    	float ret = 0.0;
     for (float i = -MOTION_BLUR; i <= MOTION_BLUR; i += 1.0)
     {
-        float t2 = t + (i / MOTION_BLUR) * 0.05;
-        ret += circleEx(p, center, exRadius, inRadius, t2, minBorder);
+        float t2 = t + (i / MOTION_BLUR) * speed * 0.1 * 0.05;
+        ret += circleEx(p, center, exRadius, inRadius, t2, speed, minBorder);
     }
     return ret / (MOTION_BLUR * 2.0 + 1.0);
 }
@@ -52,12 +55,15 @@ float circleExMB(vec2 p, vec2 center, float exRadius, float inRadius, float t, f
 float pattern(vec2 p, float t)
 {    
     float factor = (1.0 - (t / MAX_T));
-    float radius = 5.0 * factor;
+    float radius = 0.0 + 5.0 * factor;
     float width = 2.0/3.0 * factor;
     float minBorder = 0.1;
     radius = max(radius, width);
-    float tx = pow(t, 2.0);
-	float d = circleExMB(p, vec2(0.0, 1.0/3.0 - radius), radius, radius - width, tx, minBorder);
+    //radius = width;
+    radius = max(radius,1.0/3.0);
+    float tx = t;// pow(t, 2.0);
+    float speed = t;
+	float d = circleExMB(p, vec2(0.0, 1.0/3.0 - radius), radius, radius - width, tx, speed, minBorder);
     return d;
 }
 
@@ -69,11 +75,7 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     float time = mod(iGlobalTime * 0.5, MAX_T);
     //time = 0.0;
     
-    float t = iGlobalTime;
-//#if 1
-    float d = pattern(uv, time); //MB
-//#else
-//    float d = circle(uv, vec2(1.0, 0.25), 0.5, 0.375);
-//#endif
+    float d = pattern(uv, time);
+    
     fragColor = vec4(vec3(1.0 - d), 1.0);
 }
