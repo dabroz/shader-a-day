83c8e796660aa71cb3a1f835491aedbe37c72f2a
Tomasz Dąbrowski <dabroz@gmail.com>
2017-04-08 21:47:08 +0200

    Update stad0005.glsl

--- stad0005.glsl
+++ stad0005.glsl
@@ -8,8 +8,12 @@ const vec3 COLOR_1   = vec3(ivec3( 54, 194, 245)) / 255.0;
 const float PATTERN_SIZE = 0.2;
 const float PATTERN_WIDTH = 0.005;
 const float LINE_AA = 0.001;
-const float CIRCLE_SIZE = 0.08;
+const float CIRCLE_SIZE = 0.09;
     
+const float PI = 3.14159265359;
+
+const float MAX_T = 4.0;
+
 float aastep(float threshold, float value)
 {
     float aaf = fwidth(value) * 0.5;
@@ -18,13 +22,33 @@ float aastep(float threshold, float value)
 
 vec2 aastep(float threshold, vec2 value)
 {
-    return vec2(aastep(threshold, value.x), aastep(threshold, value.y));
+    vec2 aaf = fwidth(value) * 0.5;
+    return smoothstep(threshold-aaf, threshold+aaf, value);
+}
+
+vec2 warp(vec2 p, float t) 
+{    
+	float dist = max(abs(p.x), abs(p.y));
+    float quant = round(dist * 10.0) / 10.0;
+    
+    float factor = (t*3.0-1.0) ;//+ quant;
+    factor = smoothstep(0.0, 1.0, factor);
+    
+    float a = -factor * 0.5 * PI;
+    mat2 matrix = mat2(cos(a), sin(a), -sin(a), cos(a));
+    vec2 r = matrix * p;
+	return r;
 }
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     vec2 uv = 2.0 * fragCoord.xy / iResolution.xy - vec2(1.0);
     uv.x *= iResolution.x / iResolution.y;
+    
+    float t = mod(iGlobalTime, MAX_T) / MAX_T;
+    
+    vec2 baseuv = uv;
+    uv = warp(uv, t);
     
     vec2 pos = mod(uv,PATTERN_SIZE);
     vec2 rpos = vec2(PATTERN_SIZE)-pos;
@@ -36,7 +60,10 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     
     c *= aastep(CIRCLE_SIZE, length(upos));
      
-    vec3 cc = mix(COLOR_0, COLOR_BKG, c);
+    float ratio = length(baseuv);
+    vec3 color = mix(COLOR_0, COLOR_1, ratio);
+   	vec3 bkg = baseuv.x>1.6 ? vec3(step(t,baseuv.y*0.5+0.5)) : COLOR_BKG;
+    vec3 cc = mix(color, bkg, c);
     
 	fragColor = vec4(cc, 1.0);
 }
